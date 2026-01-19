1fa7b38a871889aa3e31daa50b5d34f0072f82ca
shraddha maunavalli <munavallishraddha4gmail.com>
2026-01-19 13:55:06 +0530

    save file

---
+++ miles.py
@@ -0,0 +1,8 @@
+def miles_to_kilometers(miles):
+  result = miles * 1.60934
+  return result
+
+if __name__=="__main__":
+  print("the miles to kilometers:", miles_to_kilometers(5))
+
+
---
+++ test_miles.py
@@ -0,0 +1,4 @@
+from miles import miles_to_kilometers
+
+def testcase():
+    assert miles_to_kilometers(5) == 8.04672
